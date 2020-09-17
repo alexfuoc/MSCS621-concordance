efa02a3d1ac681610c7119c09d2911a69bfa01db
alexfuoc <53452014+alexfuoc@users.noreply.github.com>
2020-09-17 19:55:19 -0400

    initial commit

---
+++ swagger_server/controllers/analysis_controller.py
@@ -0,0 +1,20 @@
+import connexion
+import six
+
+from swagger_server.models.result import Result  # noqa: E501
+from swagger_server import util
+
+
+def get_concordance(body=None):  # noqa: E501
+    """Calculate
+
+    Post text to generate concordance # noqa: E501
+
+    :param body: Text to be analyzed
+    :type body: dict | bytes
+
+    :rtype: Result
+    """
+    if connexion.request.is_json:
+        body = str.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'

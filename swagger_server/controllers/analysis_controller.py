7e834a8f080b431c39791aab7a3c2d8213f8bf88
alexfuoc <53452014+alexfuoc@users.noreply.github.com>
2020-09-18 11:05:07 -0400

    Formatted and cleaned the body input text
    Spliced, cleaned, and changed the body from bytes to a string.

--- swagger_server/controllers/analysis_controller.py
+++ swagger_server/controllers/analysis_controller.py
@@ -1,4 +1,5 @@
 import connexion
+import string
 import six
 
 from swagger_server.models.result import Result  # noqa: E501
@@ -18,9 +19,21 @@ def get_concordance(body=None):  # noqa: E501
     print("-----------------")
     print("Inside the get_concordance method")
     print(body)
+    print(type(body))
     print("-----------------")
-    print("")
 
     if connexion.request.is_json:
         body = str.from_dict(connexion.request.get_json())  # noqa: E501
+    if type(body) == bytes:
+        words = str(body, "utf-8")
+
+    print(words)
+    words = words.lower().split()
+    print("-----------------")
+    print(words)
+    #remove punctuation from each word
+    punct_table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(punct_table) for w in words]
+    print(stripped)
+
     return 'I am doing the homework!'

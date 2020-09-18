86f8b340925904f079948543a5595e74eef3eec7
alexfuoc <53452014+alexfuoc@users.noreply.github.com>
2020-09-18 12:42:29 -0400

    Finished concordance, still needs exception handling
    Returns the concordance,  updated the readme, good to go just needs some more tests and handling for none

--- swagger_server/controllers/analysis_controller.py
+++ swagger_server/controllers/analysis_controller.py
@@ -6,6 +6,33 @@ from swagger_server.models.result import Result  # noqa: E501
 from swagger_server import util
 
 
+def format_concordance(incoming_concordance={}):
+    concordance = {
+        'concordance': [
+        ]
+    }
+
+    for token, count in incoming_concordance.items():
+        concordance['concordance'].append({
+            'token': token,
+            'count': count
+        })
+
+    return concordance
+
+
+def sort_concordance(body=None):
+    concordance = {}
+
+    for word in body:
+        if word in concordance:
+            concordance[word] += 1
+        else:
+            concordance[word] = 1
+
+    return concordance
+
+
 def get_concordance(body=None):  # noqa: E501
     """Calculate
 
@@ -22,18 +49,36 @@ def get_concordance(body=None):  # noqa: E501
     print(type(body))
     print("-----------------")
 
+    # handle invalid inputs
     if connexion.request.is_json:
         body = str.from_dict(connexion.request.get_json())  # noqa: E501
     if type(body) == bytes:
         words = str(body, "utf-8")
 
-    print(words)
+    input = words
+    print(input)
+
     words = words.lower().split()
-    print("-----------------")
+    print("----------------- SPLIT")
     print(words)
-    #remove punctuation from each word
+
+    # remove punctuation from each word
     punct_table = str.maketrans('', '', string.punctuation)
     stripped = [w.translate(punct_table) for w in words]
+    print("----------------- FORMATTED")
     print(stripped)
 
-    return 'I am doing the homework!'
+    stripped.sort()
+    print("----------------- SORTED")
+    print(stripped)
+
+    concordance = sort_concordance(stripped)
+    print("----------------- CONCORDANCE")
+    print(concordance)
+
+    concordance = format_concordance(concordance)
+    concordance['input'] = input
+    print("----------------- Final Formatted CONCORDANCE")
+    print(concordance)
+
+    return concordance
